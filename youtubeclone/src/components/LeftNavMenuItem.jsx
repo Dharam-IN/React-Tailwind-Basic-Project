1f073a538813d1a8bd9eac41a9167985ad2c556e
dharamraj-dev <dharamrajyadav6312@gmail.com>
2024-02-04 09:16:09 +0530

    Add Left Navbar

--- youtubeclone/src/components/LeftNavMenuItem.jsx
+++ youtubeclone/src/components/LeftNavMenuItem.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const LeftNavMenuItem = () => {
+const LeftNavMenuItem = ({text, icon, action, className}) => {
   return (
-    <div>LeftNavMenuItem</div>
+    <div className={'text-white cursor-pointer text-sm h-10 flex items-center px-3 mb-1 rounded-lg hover:bg-white/[0.15] ' + className} onClick={action}>
+      <span className='text-xl mr-5 flex items-center gap-2'>{icon}</span>
+      {text}
+    </div>
   )
 }
 
